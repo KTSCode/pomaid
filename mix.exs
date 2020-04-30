69f87c4e8c2aba71e43c4171c74f4a388e7ccec3
Kyle San Clemente <ksanclemente@acorns.com>
2020-04-30 16:18:05 -0700

    Suit up

--- mix.exs
+++ mix.exs
@@ -3,6 +3,13 @@ defmodule Pomaid.MixProject do
 
   def project do
     [
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
       app: :pomaid,
       version: "0.1.0",
       elixir: "~> 1.10",
@@ -21,6 +28,12 @@ defmodule Pomaid.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:recon_ex, "~> 0.9.1"},
+      {:git_hooks, "~> 0.4.1", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.12.3", only: :test},
+      {:credo, "~> 1.4.0", only: :dev, runtime: false},
+      {:mix_test_watch, "~> 1.0.2", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.21.3", only: :dev, runtime: false},
       {:ratatouille, "~> 0.5.1"},
       {:dep_from_hexpm, "~> 0.3.0"}
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
