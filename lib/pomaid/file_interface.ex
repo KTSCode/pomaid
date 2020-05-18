e1cbee0346fd775fbe61d4b472a0f7c30f948cae
Kyle San Clemente <ksanclemente@acorns.com>
2020-05-18 14:15:18 -0700

    Add FileInterface module

--- lib/pomaid/file_interface.ex
+++ lib/pomaid/file_interface.ex
@@ -4,5 +4,28 @@ defmodule Pomaid.FileInterface do
     provides functions for reading and writing to files, specifically at todo.txt file and a done.txt file
   """
 
-  def hello, do: "world"
+  def read_todo_txt(path) do
+    case File.read(path) do
+      {:error, e} ->
+        {:error, e}
+
+      {:ok, contents} ->
+        {:ok, parse_todo_txt(contents)}
+    end
+  end
+
+  @doc ~S"""
+   Takes a  raw file and splits it by lines, trimming whitespace and removing blank lines
+   ## Examples
+
+          iex> Pomaid.FileInterface.parse_todo_txt("one\n   two\n\n")
+          ["one", "two"]
+
+  """
+  def parse_todo_txt(raw_file_contents) do
+    raw_file_contents
+    |> String.split("\n")
+    |> Enum.map(&String.trim/1)
+    |> Enum.filter(&(&1 != ""))
+  end
 end
--- test/pomaid/file_interface_test.exs
+++ test/pomaid/file_interface_test.exs
@@ -1,10 +1,35 @@
 defmodule Pomaid.FileInterfaceTest do
   use ExUnit.Case, async: true
+  doctest Pomaid.FileInterface
   import Pomaid.FileInterface
 
-  describe "test" do
-    test "starts interface" do
-      assert hello() == "world"
+  describe "read_todo_txt" do
+    test "when the file doesn't exist, it returns an error tuple" do
+      assert read_todo_txt("imaginary_file.txt") == {:error, :enoent}
+    end
+
+    test "when the file does exist, it returns an ok tuple with the file parsed as a list" do
+      {:ok, file} = File.open("todo.txt", [:write])
+
+      IO.binwrite(file, """
+        (A) Thank Mom for the meatballs @phone
+        (B) Schedule Goodwill pickup +GarageSale @phone
+        Post signs around the neighborhood +GarageSale
+        @GroceryStore Eskimo pies
+      """)
+
+      File.close(file)
+
+      assert read_todo_txt("todo.txt") ==
+               {:ok,
+                [
+                  "(A) Thank Mom for the meatballs @phone",
+                  "(B) Schedule Goodwill pickup +GarageSale @phone",
+                  "Post signs around the neighborhood +GarageSale",
+                  "@GroceryStore Eskimo pies"
+                ]}
+
+      File.rm("todo.txt")
     end
   end
 end
