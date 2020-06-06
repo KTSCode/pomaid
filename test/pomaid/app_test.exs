3529a529399fb09109d3652e3add548662087d81
Kyle San Clemente <ksanclemente@acorns.com>
2020-06-05 22:24:15 -0700

    add render test

--- test/pomaid/app_test.exs
+++ test/pomaid/app_test.exs
@@ -23,4 +23,75 @@ defmodule Pomaid.AppTest do
       end
     end
   end
+
+  # TODO: figure out somewhere else to put the output
+  describe "render" do
+    test "The app looks correct" do
+      with_mock Pomaid.FileInterface,
+        read_todo_file: fn _filename -> {:ok, ["task 1", "task 2"]} end,
+        select_todo_file: fn -> {:ok, "todo.txt"} end do
+        assert render(%{todo: ["task 1", "task 2"]}) == %Ratatouille.Renderer.Element{
+                 attributes: %{},
+                 children: [
+                   %Ratatouille.Renderer.Element{
+                     attributes: %{
+                       attributes: [:bold, :underline],
+                       background: :green,
+                       color: :black,
+                       content: "Pomaid (q to quit, r to reload)"
+                     },
+                     children: [],
+                     tag: :label
+                   },
+                   %Ratatouille.Renderer.Element{
+                     attributes: %{},
+                     children: [
+                       %Ratatouille.Renderer.Element{
+                         attributes: %{size: 4},
+                         children: [
+                           %Ratatouille.Renderer.Element{
+                             attributes: %{title: "To Do:"},
+                             children: [
+                               %Ratatouille.Renderer.Element{
+                                 attributes: %{title: {:todo, ["task 1", "task 2"]}},
+                                 children: [],
+                                 tag: :panel
+                               }
+                             ],
+                             tag: :panel
+                           }
+                         ],
+                         tag: :column
+                       },
+                       %Ratatouille.Renderer.Element{
+                         attributes: %{size: 4},
+                         children: [
+                           %Ratatouille.Renderer.Element{
+                             attributes: %{title: "Doing:"},
+                             children: [],
+                             tag: :panel
+                           }
+                         ],
+                         tag: :column
+                       },
+                       %Ratatouille.Renderer.Element{
+                         attributes: %{size: 4},
+                         children: [
+                           %Ratatouille.Renderer.Element{
+                             attributes: %{title: "Done:"},
+                             children: [],
+                             tag: :panel
+                           }
+                         ],
+                         tag: :column
+                       }
+                     ],
+                     tag: :row
+                   }
+                 ],
+                 tag: :view
+               }
+      end
+    end
+  end
 end
